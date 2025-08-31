b4075110c6d0ad6e194cbc3c8206794448fd2132
Nemanjavit <vitnemanja3@gmail.com>
2025-08-30 22:01:43 -0600

    simplify hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,26 +6,28 @@ import email from "../assets/email.png";
 const Hero = () => {
   return (
     <div className="hero">
-      <img src={person} alt="person" />
-      <div className="divider"></div>
-      <div className=" d-flex justify-content-md-between justify-content-lg-start flex-lg-column overlay">
-        <div className="d-flex flex-column justify-content-end justify-content-lg-center wrapper h-100">
+      <div className="shape"></div>
+      <div className="overlay"></div>
+      <div className="container hero-container">
+        <img src={person} className="profile-img hidden-img" alt="person" />
+        <img src={person} className="profile-img" alt="person" />
+        <div className="text-content">
           <div className="d-flex flex-column info">
             <span className="text-xl fw-bold">Hi, I am</span>
             <span className="text-xxl fw-bold">Nemanja Vit</span>
             <span className="text-md fw-bold">Front End Developer</span>
           </div>
-        </div>
-        <div className="d-flex flex-column flex-lg-row justify-content-end justify-content-lg-start gap-5  social">
-          <a className="" href="">
-            <img src={github} alt="github icon" />
-          </a>
-          <a className="" href="">
-            <img src={linkedIn} alt="linkedin icon" />
-          </a>
-          <a className="" href="">
-            <img src={email} alt="email icon" />
-          </a>
+          <div className="social">
+            <a className="" href="">
+              <img src={github} alt="github icon" />
+            </a>
+            <a className="" href="">
+              <img src={linkedIn} alt="linkedin icon" />
+            </a>
+            <a className="" href="">
+              <img src={email} alt="email icon" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
